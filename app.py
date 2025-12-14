d1c15761b63a0e44541c90522c0e6c0c7d13dff2
Oskar Konitz <oskar.konitz@gmail.com>
2025-12-14 12:32:50 +0100

    added week function

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import uuid
 import argparse
 from storage import load, save
 from planner import plan
+from datetime import date, timedelta
 
 def new_id(prefix):
     return f"{prefix}_{uuid.uuid4().hex[:8]}"
@@ -35,9 +36,45 @@ def cmd_add(data):
             "scheduled_date": None
         })
 
+def cmd_week(data):
+    today = date.today()
+    print("\nPLAN NA NAJBLIŻSZY TYDZIEN:")
+
+    for i in range(7):
+        day = today + timedelta(days=i)
+        day_str = day.isoformat()
+        print(f"\n{day}")
+        found = False
+        for exam in data["exams"]:
+            if exam["date"] == day_str:
+                print(f" * {exam["title"].upper()} | {exam["subject"]}")
+        for topic in data["topics"]:
+            if topic["status"] == "todo" and topic["scheduled_date"] == day_str:
+                for exam in data["exams"]:
+                    if exam["id"] == topic["exam_id"]:
+                        print(f" -{topic["id"]} {exam["subject"]} - {topic["name"]}")
+                        found = True
+                        break
+        if not found:
+            print("   brak")
+
+def cmd_done(data, t_id):
+    for topic in data["topics"]:
+        if topic["id"] == t_id:
+            topic["status"] = "done"
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("cmd", choices=["add","plan"])
+    # parser.add_argument("cmd", choices=["add","plan","week"])
+    sub = parser.add_subparsers(dest="cmd", required=True)
+
+    sub.add_parser("add")
+    sub.add_parser("plan")
+    sub.add_parser("week")
+
+    p_done = sub.add_parser("done")
+    p_done.add_argument("topic_id")
+
     args = parser.parse_args()
 
     data = load()
@@ -48,6 +85,12 @@ def main():
     if args.cmd == "plan":
         plan(data)
         print("planowanie zakonczone")
+
+    if args.cmd == "week":
+        cmd_week(data)
+
+    if args.cmd == "done":
+        cmd_done(data, args.topic_id)
 
     save(data)
 
