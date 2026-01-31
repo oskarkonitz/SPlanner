0aeded1aed2e7cda686ef7eff871ee9fea9c0b9e
Oskar Konitz <oskar.konitz@gmail.com>
2026-01-31 22:57:18 +0100

    Wersja 1.0.1 - test builda

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from gui.effects import ConfettiEffect, FireworksEffect
 from gui.windows.timer import TimerWindow
 from gui.windows.achievements import AchievementManager
 
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 class GUI:
     def __init__(self, root):
